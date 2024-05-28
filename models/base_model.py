21b035d5f6f4d8174e26b55226c648e5111ad38b
engfatmait <eng_fatma_it@yahoo.com>
2024-05-28 09:41:06 -0700

    new commit

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Parent class that will inherit
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,40 +2,43 @@
 
 """ module of unittests """
 import unittest
+import models
 from models.base_model import BaseModel
 
 
 class BaseModelTests(unittest.TestCase):
     """ Suite of Console Tests """
 
-    my_model = basemodel();
+    my_model = BaseModel()
 
     def test_init(self):
         """ Test attributes value of init """
+        my_model = BaseModel()
         self.assertIsNotNone(my_model.id)
         self.assertIsNotNone(my_model.created_at)
         self.assertIsNotNone(my_model.updated_at)
 
     def test_save(self):
         """ Test attributes value of a BaseModel instance """
-
+        my_model = BaseModel()
         intial_updated_at = my_model.updated_at
         currnt_updated_at = my_model.save()
 
-        self.assertEqual(intial_updated_at, currnt_updated_at)
+        self.assertNotEqual(intial_updated_at, currnt_updated_at)
 
     def test_to_dict(self):
 
         """ test attributes value """
-
+        my_model = BaseModel()
         my_model_dict = my_model.to_dict()
-        self.assertEqual(my_model_dict["__class__"], "basemodel")
+        self.assertEqual(my_model_dict["__class__"], "BaseModel")
         self.assertEqual(my_model_dict["id"], my_model.id)
         self.assertEqual(my_model_dict["created_at"], my_model.created_at.isoformat())
-        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at)
 
     def test_str(self):
-        self.assertTrue(str(my_model).starttwith('[basemodel]'))
+        my_model = BaseModel()
+        self.assertTrue(str(my_model).startswith('[BaseModel]'))
 
 
 if __name__ == "__main__":
