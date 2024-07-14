d73b0dabe43bfab62a97e08d455d4fc76a9569ad
engfatmait <eng_fatma_it@yahoo.com>
2024-07-14 02:02:30 -0700

    new commit

--- console.py
+++ console.py
@@ -118,25 +118,24 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         else:
              objects = storage.all()
+             key = "{}.{}".format(commands[0], commands[1])
+             if key not in objects:
+                print("** no instance found **")
+             elif len(commands) < 3:
+                print ("** attribute name missing **")
+             elif len(commands) < 4:
+                print ("** value missing **")
+             else:
+                obj = objects[key]
+                attr_name = commands[2]
+                attr_value = commands[3]
 
-        key = "{}.{}".format(commands[0], commands[1])
-        if key not in objects:
-            print("** no instance found **")
-        elif len(commands) < 3:
-            print ("** attribute name missing **")
-        elif len(commands) < 4:
-            print ("** value missing **")
-        else:
-             obj = objects[key]
-             attr_name = commands[2]
-             attr_value = commands[3]
-
-        try:
-            attr_value = eval(attr_value)
-        except exception:
-            pass
-        setattr(obj, attr_name, attr_value)
-        obj.save()
+                try:
+                    attr_value = eval(attr_value)
+                except exception:
+                    pass
+                setattr(obj, attr_name, attr_value)
+                obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
