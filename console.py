16ccecbe05355b17e5c8760fd769d25b9324c855
engfatmait <eng_fatma_it@yahoo.com>
2024-07-15 01:30:51 -0700

    new commit

--- console.py
+++ console.py
@@ -7,12 +7,17 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb)"
-    valid_classes =["BaseModel", "User"]
+    valid_classes =["BaseModel", "User", "Place", "State", "City", "Amenity", "Review"]
 
     def do_quit(self, arg):
         """
--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,9 @@ import models
 class BaseModel:
     def __init__(self, *args, **kwargs):
         date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -17,10 +20,6 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, date_format))
                 else:
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
         models.storage.new(self)
 
     def save(self):
